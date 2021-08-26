c14bb08d3c0dbddf67442428c3ca257f27ef1004
Axel Brionne <axel.brionne@gmail.com>
2021-08-26 11:01:22 +0200

    feat: add Custom Authenticator for check JWT Token

--- src/Utils/JwtUtils.php
+++ src/Utils/JwtUtils.php
@@ -3,12 +3,21 @@
 namespace App\Utils;
 
 use App\Entity\User;
+use Jose\Component\Checker\AlgorithmChecker;
+use Jose\Component\Checker\ClaimCheckerManager;
+use Jose\Component\Checker\ExpirationTimeChecker;
+use Jose\Component\Checker\HeaderCheckerManager;
+use Jose\Component\Checker\IssuedAtChecker;
 use Jose\Component\Core\AlgorithmManager;
 use Jose\Component\Core\JWK;
 use Jose\Component\KeyManagement\JWKFactory;
 use Jose\Component\Signature\Algorithm\RS256;
 use Jose\Component\Signature\JWSBuilder;
+use Jose\Component\Signature\JWSLoader;
+use Jose\Component\Signature\JWSTokenSupport;
+use Jose\Component\Signature\JWSVerifier;
 use Jose\Component\Signature\Serializer\CompactSerializer;
+use Jose\Component\Signature\Serializer\JWSSerializerManager;
 
 class JwtUtils {
     private const PROJECT_DIRECTORY = '..'.DIRECTORY_SEPARATOR;
@@ -45,5 +54,43 @@ class JwtUtils {
         $serializer = new CompactSerializer(); // The serializer
 
         return $serializer->serialize($jws, 0);
+    }
+
+    public static function verify(string $token): bool {
+        $algorithmManager = new AlgorithmManager([
+            new RS256(),
+        ]);
+
+        $jwsVerifier = new JWSVerifier(
+            $algorithmManager
+        );
+
+        $jwk = JWKFactory::createFromKeyFile(
+            self::PROJECT_DIRECTORY.$_ENV['JWT_PUBLIC_KEY'],
+            $_ENV['JWT_PASSPHRASE']
+        );
+        $serializerManager = new JWSSerializerManager([new CompactSerializer()]);
+        
+        $claimCheckerManager = new ClaimCheckerManager(
+            [
+                new IssuedAtChecker(),
+                new ExpirationTimeChecker(),
+            ]
+        );
+
+        $jws = $serializerManager->unserialize($token);
+        $claims = json_decode($jws->getPayload(), true);
+        
+        return $jwsVerifier->verifyWithKey($jws, $jwk, 0) && $claimCheckerManager->check($claims);
+    }
+
+    public static function decode($token): User {
+        $serializerManager = new JWSSerializerManager([
+            new CompactSerializer(),
+        ]);
+        $jws = $serializerManager->unserialize($token);
+        $user = new User();
+        $user->setUsername(json_decode($jws->getPayload())->username);
+        return $user;
     }
 }
